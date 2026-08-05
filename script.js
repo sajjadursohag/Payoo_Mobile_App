d67fe10b3babfb63d13191780999a0c71f452ebf
sajjadursohag <sajjadursub07@gmail.com>
2026-08-06 00:07:24 +0600

    added home page

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // login button functionally
 
 document.getElementById("loginButton").addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("Login button clicked");
+    e.preventDefault();
+    
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid Credentials");
+  }
 });
