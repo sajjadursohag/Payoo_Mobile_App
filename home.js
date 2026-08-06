57bb6e957ed51695cea70b767d477ef8cbaef008
sajjadursohag <sajjadursub07@gmail.com>
2026-08-06 07:07:44 +0600

    added home.js functionally

---
+++ home.js
@@ -0,0 +1,12 @@
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("Add money received");
+
+    const bank = doucment.getElementById("bank").value;
+    const accountNumber = document.getElementById("account-number").value;
+    const amount = parseInt(document.getElementById("add-amount").value);
+
+    const pin;
+  });
