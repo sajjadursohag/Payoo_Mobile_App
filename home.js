f5637ec928223ba54aabeb95a74a143b55a270f1
sajjadursohag <sajjadursub07@gmail.com>
2026-08-07 00:04:38 +0600

    added implement pin

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 1234;
+
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -8,12 +10,25 @@ document
     const accountNumber = document.getElementById("account-number").value;
     const amount = parseInt(document.getElementById("add-amount").value);
 
-    const pin = document.getElementById("add-pin").value;
+    const pin = parseInt(document.getElementById("add-pin").value);
+
     const avilableBalance = parseInt(
       document.getElementById("availble-balance").innerText,
     );
 
-    console.log(avilableBalance);
+    console.log(amount, avilableBalance);
+
+    if (accountNumber.length < 11) {
+      alert("please provide valid account number");
+      return;
+    }
+
+    if (pin !== validPin) {
+      alert("please provid valid pin number ");
+      return;
+    }
+
+    
 
     const totalNewAvailableBalance = amount + avilableBalance;
 
